3635164978ef6e8274debf25fdfa3275daae05c8
TranLongVu-DepTrai <vutl.22it@vku.udn.vn>
2025-01-12 23:10:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using OOP_HomeWork_1;
+
+namespace OOP_HomeWork_1
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Nhap so mat cua xxc xac: ");
+            int soMat = int.Parse(Console.ReadLine());
+
+
+            XucXac Xucxac = new XucXac(soMat);
+
+          
+            Console.WriteLine($"Kết quả của lần tung xúc xắc: {Xucxac.DoXucXac()}");
+        }
+    }
+}
